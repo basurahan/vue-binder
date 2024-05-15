f989ca58adcd423df3c791b9a4bcae9039837b94
Renz <dev.renzsalanga@gmail.com>
2024-05-15 11:03:53 +0800

    refactor: code cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,11 +38,10 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.bundles.core.android)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.navigation.fragment)
+    implementation(libs.recycerview)
+
     implementation(project(":vue-binder"))
 }
--- vue-binder/build.gradle.kts
+++ vue-binder/build.gradle.kts
@@ -46,7 +46,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.basurahan"
             artifactId = "vue-binder"
-            version = "1.0.0"
+            version = "1.4.0"
 
             afterEvaluate {
                 from(components.getAt("release"))
